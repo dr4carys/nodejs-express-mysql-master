bfa0b35b478d7745d463c3d6f7cd2b634c52e64a
dr4carys <48232195+dr4carys@users.noreply.github.com>
2021-09-25 11:03:10 +0800

    first commit

---
+++ app/routes/customer.routes.js
@@ -0,0 +1,24 @@
+module.exports = app => {
+  const customers = require("../controllers/customer.controller.js");
+
+  // Create a new Customer
+  app.post("/customers", customers.create);
+
+  // Retrieve all Customers
+  app.get("/customers", customers.findAll);
+  // app.get("/ambildata/:userId",user.findOne);
+
+  // Retrieve a single Customer with customerId
+  app.get("/customers/:customerId", customers.findOne);
+
+  // Update a Customer with customerId
+  app.put("/customers/:customerId", customers.update);
+
+  // Delete a Customer with customerId
+  app.delete("/customers/:customerId", customers.delete);
+
+  // Create a new Customer
+  app.delete("/customers", customers.deleteAll);
+  
+  app.post("/admin/signin",customers.signIn);
+};
